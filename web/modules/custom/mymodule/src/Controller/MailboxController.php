0357299f0210c0cdc1acd0dbca0cb01b47ce3c99
Debjyoti Saha <debjyoti.saha@innoraft.com>
2021-07-09 13:21:20 +0530

    TRN-433: Added docblocks

--- web/modules/custom/mymodule/src/Controller/MailboxController.php
+++ web/modules/custom/mymodule/src/Controller/MailboxController.php
@@ -5,22 +5,40 @@ use Drupal\Core\Config\ConfigFactory;
 use Drupal\Core\DependencyInjection\ContainerInjectionInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
-
+/**
+ * MailboxController class
+ * used to show credentials of mailboxlayer
+ */
 class MailboxController implements ContainerInjectionInterface{
     protected $config;
+    /**
+     * @inheritDoc
+     *
+     * @param ConfigFactory $config
+     */
     public function __construct(ConfigFactory $config)
     {
         $this->config=$config;
     }
+
+    /**
+     * @inheritDoc
+     *
+     * @param ContainerInterface $container
+     */
     public static function create(ContainerInterface $container) {
         return new static(
           $container->get('config.factory')
         );
       }
+
+    /**
+     * Shows the Client ID from mymodule.settings
+     */
     public function showCredentials() {
         $access_key = $this->config->get('mymodule.settings')->get('config');
         return array (
-            '#markup' => '<b>Client ID:<b> '.$access_key
+            '#markup' => '<b>Client ID:</b> '.$access_key
         );
     }
 }
